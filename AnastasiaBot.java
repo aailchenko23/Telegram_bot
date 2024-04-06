9eb01bf11e52032cf33635ae6c74a8c2a8978c51
aailchenko23 <125138023+aailchenko23@users.noreply.github.com>
2024-04-06 16:57:42 +0300

    Update AnastasiaBot.java

--- AnastasiaBot.java
+++ AnastasiaBot.java
@@ -27,29 +27,27 @@ public class AnastasiaBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        if (update.hasMessage() && update.getMessage().hasText()) {
-            String messageText = update.getMessage().getText();
-            String chatId = update.getMessage().getChatId().toString();
-            SendMessage sendMessage = new SendMessage();
-            sendMessage.setChatId(chatId);
-            if (messageText.equals("/start")) {
-                String startMessage = "Welcome to Beauty Salon Bot!\n" +
-                        "Please select a service:";
-                sendMessage.setText(startMessage);
-                sendMessage.setReplyMarkup(getServiceKeyboard());
-            } else if (messageText.equalsIgnoreCase("website")) {
-                String websiteLink = "https://https://www.instagram.com/aa.ilchenko?igsh=ODd6enp4aXZ3eHg1&utm_source=qr";
-                sendMessage.setText("Here is our website: " + websiteLink);
-            } else {
-                String reply = getServicePrice(messageText);
-                sendMessage.setText(reply);
-            }
+       String messageText = update.getMessage().getText();
+        String chatId = update.getMessage().getChatId().toString();
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatId);
+        if (messageText.equals("/start")) {
+            String startMessage = "Ласкаво просимо до Beauty Salon Bot!\n" +
+                    "Будь ласка, оберіть послугу:";
+            sendMessage.setText(startMessage);
+            sendMessage.setReplyMarkup(getServiceKeyboard());
+        } else if (messageText.equalsIgnoreCase("website")) {
+            String websiteLink = "https://https://www.instagram.com/aa.ilchenko?igsh=ODd6enp4aXZ3eHg1&utm_source=qr";
+            sendMessage.setText("Ось наш веб-сайт: " + websiteLink);
+        } else {
+            String reply = getServicePrice(messageText);
+            sendMessage.setText(reply);
+        }
 
-            try {
-                execute(sendMessage);
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
-            }
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
         }
     }
 
@@ -61,13 +59,13 @@ public class AnastasiaBot extends TelegramLongPollingBot {
 
         List<KeyboardRow> keyboardRows = new ArrayList<>();
         KeyboardRow row1 = new KeyboardRow();
-        row1.add("Hairstyle");
-        row1.add("Haircut");
+        row1.add("Зачіска");
+        row1.add("Стрижка");
         KeyboardRow row2 = new KeyboardRow();
-        row2.add("Makeup");
-        row2.add("Manicure");
+        row2.add("Макіяж");
+        row2.add("Манікюр");
         KeyboardRow row3 = new KeyboardRow();
-        row3.add("Website");
+        row3.add("Веб-сайт");
 
         keyboardRows.add(row1);
         keyboardRows.add(row2);
@@ -77,20 +75,20 @@ public class AnastasiaBot extends TelegramLongPollingBot {
         return keyboardMarkup;
     }
 
-     private  String getServicePrice(String service) {
+    private String getServicePrice(String service) {
         switch (service.toLowerCase()) {
-            case "hairstyle":
-                return "Price for Hairstyle: $50";
-            case "haircut":
-                return "Price for Haircut: $30";
-            case "makeup":
-                return "Price for Makeup: $40";
-            case "manicure":
-                return "Price for Manicure: $20";
-            case "website":
-                return "Here is our website: https://https://www.instagram.com/aa.ilchenko?igsh=ODd6enp4aXZ3eHg1&utm_source=qr";
+            case "зачіска":
+                return "Ціна за Зачіску: $50";
+            case "стрижка":
+                return "Ціна за Стрижку: $30";
+            case "макіяж":
+                return "Ціна за Макіяж: $40";
+            case "манікюр":
+                return "Ціна за Манікюр: $20";
+            case "веб-сайт":
+                return "Ось наш веб-сайт: https://https://www.instagram.com/aa.ilchenko?igsh=ODd6enp4aXZ3eHg1&utm_source=qr";
             default:
-                return "Sorry, we don't offer this service.";
+                return "Вибачте, ми не надаємо цю послугу.";
         }
     }
 }
